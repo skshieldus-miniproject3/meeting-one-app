ace57018713ec1702484ce0fad017edd9fe51944
yunjongyun <yunyami0605@gmail.com>
2025-10-20 16:35:01 +0900

    chore: 프로젝트 초기셋팅
    1. 프로젝트 초기셋팅

---
+++ src/main/java/com/meetingoneline/meeting_one_line/MeetingOneLineApplication.java
@@ -0,0 +1,13 @@
+package com.meetingoneline.meeting_one_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingOneLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingOneLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetingoneline/meeting_one_line/MeetingOneLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingoneline.meeting_one_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingOneLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
